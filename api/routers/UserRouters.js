32481877a93419cb640b3828bb0e8d2596434380
Indo Deriano <mde50526@gmail.com>
2020-05-08 17:56:35 +0800

    Update UserRouters.js
    sign

--- api/routers/UserRouters.js
+++ api/routers/UserRouters.js
@@ -3,7 +3,6 @@ const {UserControllers}=require('../controllers')
 
 const Router=express.Router()
 
-Router.get('/allusersindo',UserControllers.allusers)
 // Router.get('/nothing',UserControllers.empty)
 
 module.exports=Router
