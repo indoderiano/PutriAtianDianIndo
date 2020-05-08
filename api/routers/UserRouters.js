4a570309f1e684cdfd1a3362895f316819066cae
Indo Deriano <mde50526@gmail.com>
2020-05-08 17:57:06 +0800

    Update UserRouters.js

--- api/routers/UserRouters.js
+++ api/routers/UserRouters.js
@@ -3,6 +3,7 @@ const {UserControllers}=require('../controllers')
 
 const Router=express.Router()
 
+Router.get('/allusers',UserControllers.allusers)
 // Router.get('/nothing',UserControllers.empty)
 
 module.exports=Router
