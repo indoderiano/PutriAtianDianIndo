1b70c5f57979206506f31974d19710416c4c9470
Indo Deriano <mde50526@gmail.com>
2020-05-10 17:37:18 +0800

    test

--- api/routers/UserRouters.js
+++ api/routers/UserRouters.js
@@ -4,6 +4,5 @@ const {UserControllers}=require('../controllers')
 const Router=express.Router()
 
 Router.get('/allusers',UserControllers.allusers)
-// Router.get('/nothing',UserControllers.empty)
 
 module.exports=Router
