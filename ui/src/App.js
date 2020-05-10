889d3daf93e1200cfa1ad0bb6b76a9b86cf00ea1
Indo Deriano <mde50526@gmail.com>
2020-05-10 20:52:40 +0800

    home

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Register from './pages/register'
+import Home from './pages/home'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           {/* <Route path='/register' exact component={Register}/> */}
+          <Route path='/' exact component={Home}/>
           <Route path='/register' exact component={Register}/>
         </Switch>
       </BrowserRouter>
